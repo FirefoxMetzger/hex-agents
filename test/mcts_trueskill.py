2634b53e92ca1d07da2cf4b0731acf92d42d83d9
FirefoxMetzger <sebastian@wallkoetter.net>
2020-07-01 08:55:05 +0200

    rename_folder

--- test/mcts_trueskill.py
+++ test/mcts_trueskill.py
@@ -2,46 +2,22 @@ import gym
 import minihex
 import numpy as np
 from mcts.MCTSAgent import MCTSAgent
-from minihex.HexGame import HexGame, player
+from minihex import HexGame, player
 import trueskill
 import json
+from Agent import Agent, RandomAgent
 
 
-class Agent(object):
-    def __init__(self, depth=1000):
-        self.depth = depth
-        self.rating = trueskill.Rating()
-
-    def policy(self, state, active_player):
-        env = HexGame(active_player, state, active_player)
-        agent = MCTSAgent(env)
-        return agent.plan(self.depth)
-
-    def __str__(self):
-        return f"MCTS({self.depth})"
-
-
-class RandomAgent(object):
-    def __init__(self):
-        self.rating = trueskill.Rating()
-
-    def policy(self, state, active_player):
-        idx = minihex.empty_tiles(state)
-        choice = np.random.randint(len(idx))
-        return idx[choice]
-
-    def __str__(self):
-        return "Random"
-
-
-def play_match(agent1, agent2):
-    env = gym.make("hex-v0", opponent_policy=agent2.policy)
-    state = env.reset()
+def play_match(agent1, agent2, sim=None):
+    env = gym.make("hex-v0", opponent_policy=agent2.act, board_size=9)
+    state, info = env.reset()
+    agent1.reset(sim)
+    agent2.reset(sim)
 
     done = False
     while not done:
-        action = agent1.policy(*state)
-        state, reward, done, _ = env.step(action)
+        action = agent1.act(*state, info)
+        state, reward, done, info = env.step(action)
 
     if reward == -1:
         agent2.rating, agent1.rating = trueskill.rate_1vs1(agent2.rating,
@@ -52,19 +28,19 @@ def play_match(agent1, agent2):
     else:
         agent2.rating, agent1.rating = trueskill.rate_1vs1(agent2.rating,
                                                            agent1.rating,
-                                                           draw=True)
+                                                           drawn=True)
 
 
 def find_match(player1, agent_pool):
     match_quality = list()
-    eligable_opponent = list()
+    eligible_opponent = list()
     for agent in agent_pool:
         if agent == player1:
             continue
         else:
             match_quality.append(trueskill.quality_1vs1(player1.rating,
                                                         agent.rating))
-            eligable_opponent.append(agent)
+            eligible_opponent.append(agent)
     match_quality = np.exp(match_quality)/sum(np.exp(match_quality))
     return np.random.choice(eligible_opponent, p=match_quality)
 
@@ -75,18 +51,21 @@ trueskill.setup(mu=1200,
                 tau=2.0,
                 draw_probability=0.01,
                 backend="scipy")
-agent_pool = [Agent(depth=d) for d in [100, 500, 1000, 1500]]
+env = gym.make("hex-v0", opponent_policy=None, board_size=9)
+env.reset()
+sim = env.simulator
+agent_pool = [MCTSAgent(sim, depth=d) for d in [10, 50, 100, 500, 1000, 1500]]
 agent_pool += [RandomAgent()]
-for game_idx in range(100):
+
+for game_idx in range(1000):
     # pick the agent we are most uncertain about
     sigmas = [agent.rating.sigma for agent in agent_pool]
     player1_idx = np.argmax(sigmas)
     player1 = agent_pool[player1_idx]
     player2 = find_match(player1, agent_pool)
-    play_match(player1, player2)
-
     print("---")
     print(f"MATCH {game_idx}: {player1} vs. {player2}")
+    play_match(player1, player2, sim=sim)
     leaderboard = sorted(agent_pool,
                          key=lambda x: trueskill.expose(x.rating),
                          reverse=True)
