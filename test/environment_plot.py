115643054cf9de82bd11a45eccd5de664abcbe48
FirefoxMetzger <sebastian@wallkoetter.net>
2020-07-05 20:20:54 +0200

    updated graph

--- test/environment_plot.py
+++ test/environment_plot.py
@@ -5,32 +5,35 @@ import numpy as np
 
 def get_mcts_scores(file_name):
     with open(file_name, "r") as result_file:
-        result_5x5 = json.load(result_file)
+        result = json.load(result_file)
 
     x = list()
     y = list()
     ci = list()
-    for key in result_5x5.keys():
+    for key in result.keys():
         if key == "Random":
             x.append(0)
         else:
             x.append(int(key[5:-1]))
-        y.append(result_5x5[key]['mu'])
-        ci.append(result_5x5[key]['sigma'])
+        y.append(result[key]['mu'])
+        ci.append(result[key]['sigma'])
 
     idx = np.argsort(x)
     x = np.array(x)[idx]
     y = np.array(y)[idx]
     ci = np.array(ci)[idx]
 
+    # RandomAgent has no skill
+    y = y - y[0]
+
     return x, y, ci
 
 
 fig, ax = plt.subplots()
 
-x, y, ci = get_mcts_scores("mcts_eval(5x5)2.json")
+x, y, ci = get_mcts_scores("mcts_eval(5x5).json")
 x2, y2, ci2 = get_mcts_scores("mcts_eval(9x9).json")
-x3, y3, ci3 = get_mcts_scores("mcts_eval(11x11).json")
+x3, y3, ci3 = get_mcts_scores("mcts_eval(11x11)big.json")
 
 ax.plot(x, y, marker='.')
 ax.fill_between(x, (y-ci), (y+ci), color='b', alpha=.1)
